c4e9e2c6abb41f6f7863bb1f2ce56ba5774c9111
Tyler Crabb <tydacrabb@gmail.com>
2018-12-03 11:24:32 -0800

    added checks for name being string and age being number

--- drills1.js
+++ drills1.js
@@ -6,16 +6,19 @@ function yearOfBirth(age){
   return 2018 - age;
 }
 function createGreeting(name, age) {
-  if (name && age) {
+  if (name && age ) {
+    if(typeof(name) !== 'string' || typeof(age) !== 'number'){
+      throw new TypeError('name should be string, age should be positive number')
+    }
     const birthYear = yearOfBirth(age);
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${birthYear}.`;
   } else {
     throw new Error('Arguments not valid');
-  }
+  } 
 }
 
 try{
-  const greeting = createGreeting();
+  const greeting = createGreeting('tyler', '27');
   // eslint-disable-next-line no-console
   console.log(greeting);
 } catch(e){
