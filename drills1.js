6a8531859a6ae66c47033766d90e0965375d957a
Tyler Crabb <tydacrabb@gmail.com>
2018-12-03 11:13:02 -0800

    added error handling for negative age

--- drills1.js
+++ drills1.js
@@ -1,11 +1,18 @@
 'use strict';
-
-function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;
-  return `Hi, my name is ${name} and I'm ${age} years old. I was borin in ${yearOfBirth}.`;
+function yearOfBirth(age){
+  if(age < 0){
+    throw new Error('Age can not be negative')
+  }
+  return 2018 - age;
 }
-
-let greeting = createGreeting();
+function createGreeting(name, age) {
+  const birthYear = yearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${birthYear}.`;
+}
+try{
+const greeting = createGreeting('derek', -6);
+}catch(e){console.error(e);
+}
 
 // eslint-disable-next-line no-console
 console.log(greeting);
