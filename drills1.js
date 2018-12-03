6f61fb83d6f2e18b78fe91fc5d34b8683ad85ecc
Derek Houck <derek@derekhouck.com>
2018-12-03 11:17:54 -0800

    A conditional to verify parameters have been entered

--- drills1.js
+++ drills1.js
@@ -1,18 +1,24 @@
 'use strict';
 function yearOfBirth(age){
   if(age < 0){
-    throw new Error('Age can not be negative')
+    throw new Error('Age can not be negative');
   }
   return 2018 - age;
 }
 function createGreeting(name, age) {
-  const birthYear = yearOfBirth(age);
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${birthYear}.`;
-}
-try{
-const greeting = createGreeting('derek', -6);
-}catch(e){console.error(e);
+  if (name && age) {
+    const birthYear = yearOfBirth(age);
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${birthYear}.`;
+  } else {
+    throw new Error('Arguments not valid');
+  }
 }
 
-// eslint-disable-next-line no-console
-console.log(greeting);
+try{
+  const greeting = createGreeting();
+  // eslint-disable-next-line no-console
+  console.log(greeting);
+} catch(e){
+  // eslint-disable-next-line no-console
+  console.error(e);
+}
