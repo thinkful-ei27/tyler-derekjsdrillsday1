da4353059f3d158d2a175dc6111584634cbf4402
Tyler Crabb <tydacrabb@gmail.com>
2018-12-03 10:56:28 -0800

    changed to return greeting

--- drills1.js
+++ drills1.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line strict
 function createGreeting() {
-  // eslint-disable-next-line no-console
-  console.log('Hi, my name is Tyler and I\'m 27 years old');
+  return 'Hi, my name is Tyler and I\'m 27 years old';
 }
+let greeting = createGreeting();
+console.log(greeting);
 
-createGreeting();
+
